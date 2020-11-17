c8093a04a31a8e9d9df4c806c2049f109f876abd
Jimmy Ljungberg <jimmy.ljungberg@gmail.com>
2020-11-17 19:37:05 +0100

    Introducera klassen Position

---
+++ domain/Entity/src/Entity/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Domain.Entity {
+	public class Position {
+		private float _x;
+		private float _y;
+		private float _z;
+		public Position(float x, float y, float z) {
+			this._x = x;
+			this._y = y;
+			this._z = z;
+		}
+
+		public float x => _x;
+		public float y => _y;
+		public float z => _z;
+	}
+}
